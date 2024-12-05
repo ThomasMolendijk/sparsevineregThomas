7a00b8ac996d540b6cd8318d30c209877f13a0a8
ThomasMolendijk <82886109+ThomasMolendijk@users.noreply.github.com>
2024-12-05 10:11:22 +0100

    Update est_Resid.R

--- R/est_Resid.R
+++ R/est_Resid.R
@@ -9,7 +9,7 @@ est_resid <- function(resp, cov, name_cnd=NA, alpha=NA, order=NA, cores_vine){
     res <- list("fit"=fit_upd)
   }
   else{
-    fit_upd <- vinereg::vinereg(y ~ ., data=data_upd, order=names(data_upd)[-1], cores=cores_vine)
+    fit_upd <- vinereg::vinereg(y ~ ., data=data_upd, family_set="all", order=names(data_upd)[-1], cores=cores_vine)
     pred_upd <- vinereg:::predict.vinereg(fit_upd, newdata = data_upd, alpha=alpha)
     resid <- resp - pred_upd[,1]
     res <- list("resid"=resid, "fit"=fit_upd)
