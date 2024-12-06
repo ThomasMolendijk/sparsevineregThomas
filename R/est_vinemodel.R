b517ff7374b27a201b1908418e7962b983929840
ThomasMolendijk <82886109+ThomasMolendijk@users.noreply.github.com>
2024-12-06 11:25:52 +0100

    Update est_vinemodel.R

--- R/est_vinemodel.R
+++ R/est_vinemodel.R
@@ -4,7 +4,7 @@
 update_mdl <- function(resp, cov, name_cnd=NA, cores_vine){
   data_upd <- data.frame(y = resp, cov)
   colnames(data_upd)[2:ncol(data_upd)] <- name_cnd
-  fit_upd <- vinereg::vinereg(y ~ ., data=data_upd, order=names(data_upd)[-1], cores=cores_vine)
+  fit_upd <- vinereg::vinereg(y ~ ., data=data_upd, family_set="all", order=names(data_upd)[-1], cores=cores_vine)
   res <- list("fit"=fit_upd)
   res
 }
